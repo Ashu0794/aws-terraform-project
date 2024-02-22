bfff7b2b584f28bd18210eada2e7170037d9f36a
Ashu0794 <ashutosh.mishra021@gmail.com>
2024-02-23 03:22:32 +0530

    First Commit

---
+++ Webserver/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region     = var.region
+  access_key = "AKIARSX3EA4TZGD5G5PK"
+  secret_key = "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"
+}
+
+#create vpc
+module "vpc" {
+  source                       = "../modules/vpc"
+  region                       = var.region
+  project_name                 = var.project_name
+  vpc_cidr                     = var.vpc_cidr
+  public_subnet_az1_cidr       = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr       = var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+
+
+
+}
