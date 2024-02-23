95706e45ac0f778f4e583be9b3422ccc293f44bb
Ashu0794 <ashutosh.mishra021@gmail.com>
2024-02-23 11:32:40 +0530

    NAT Gateway

--- Webserver/backend.tf
+++ Webserver/backend.tf
@@ -1,8 +1,8 @@
 # store the terraform state file in s3
 terraform {
   backend "s3" {
-    bucket    = "terra-back-ashu"
-    key       = "webserver.tfstate"
-    region    = "us-west-2"
+    bucket = "terra-back-ashu"
+    key    = "webserver.tfstate"
+    region = "us-west-2"
   }
 }
